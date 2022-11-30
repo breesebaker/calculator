c10a50999cf868ba735097c97f84d661d39a82df
Christopher Baker <cbreesebaker@gmail.com>
2022-11-30 08:47:33 -0600

    Committing Js scripts

--- calculator.js
+++ calculator.js
@@ -1,3 +1,10 @@
+const display = document.querySelector(".calculator-display");
+const buttons = Array.from(document.querySelectorAll(".calculator-buttons"));
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const calcButton = e.target.innerText;
+    const calcDisplay = display.innerText;
 
     if(calcButton === "AC") {
       display.innerText = " ";
